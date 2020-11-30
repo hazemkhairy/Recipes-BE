65b35dea9399c89c6f650d7924a01932e3a96357
hazemkhairy <hazemkhairy188@gmail.com>
2020-11-30 02:16:18 +0200

    - Added Dummy Recipes data - Added addRecipe Endpoint & Logic - Added getAllRecipes Endpoint & Logic

---
+++ RecipesTempData.js
@@ -0,0 +1,36 @@
+module.exports= {
+    data:[
+        {
+            id: '13js12',
+            name: 'Potoato-1',
+            descripition: 'do this and that',
+            imageURL: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'
+        }, {
+            id: '132214sd',
+            name: 'Potoato-2',
+            descripition: 'do this and that',
+            imageURL: 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?fit=1200%2C879&ssl=1'
+        }, {
+            id: '123jsaod12-3',
+            name: 'Potoato',
+            descripition: 'do this and that',
+            imageURL: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1274503_8-8d8dfb5.jpg?quality=90&resize=960,872'
+        }
+        , {
+            id: '123jsaod1',
+            name: 'Cheese Cake1',
+            descripition: '7elw',
+            imageURL: 'https://images-gmi-pmc.edge-generalmills.com/1e24b5e7-e3e3-43ce-b737-a2215397f006.jpg'
+        }, {
+            id: '123jsaod1',
+            name: 'Cheese Cake2',
+            descripition: '7elw',
+            imageURL: 'https://images-gmi-pmc.edge-generalmills.com/1e24b5e7-e3e3-43ce-b737-a2215397f006.jpg'
+        }, {
+            id: '123jsaod1',
+            name: 'Cheese Cake',
+            descripition: '7elw',
+            imageURL: 'https://images-gmi-pmc.edge-generalmills.com/1e24b5e7-e3e3-43ce-b737-a2215397f006.jpg'
+        }
+    ]
+}
--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = 3000
 
+//dummy data until adding database
+const recipes = require('./RecipesTempData').data
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Headers","*");
+    res.setHeader("Access-Control-Allow-Methods","*");
+    next();
+})
+
+
+app.get('/api/recipes', (req, res) => {
+    res.status(200).json({message:'success',recipes})
+})
+app.post('/api/addRecipe', (req, res) => {
+    let recipe = req.body
+    recipes.push(recipe);
+    res.status(201).json({message:'Added successfully'});
+})
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.end('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
