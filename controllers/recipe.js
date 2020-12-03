39118fe8dda4b3968079e408e97cd2cf5b6f3760
hazemkhairy <hazemkhairy188@gmail.com>
2020-12-03 02:19:12 +0200

    -Added getRecipe -Added some error handling

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -2,19 +2,50 @@ const Recipe = require('../model/recipe');
 
 
 const getAllRecipes = async (req, res) => {
-    let recipes = await Recipe.find();
-    res.status(200).json({ message: 'success', recipes })
+    Recipe.find().then(
+        (recipes) => {
+            res.status(200).json({ message: 'Success', recipes })
+        }
+    )
+        .catch(
+            () => {
+                res.status(404).json({ message: 'Couldn\'t retrieve Recipes' })
+            }
+        )
 }
 
-const addRecipe = (req, res) => {
+const getRecipe = async (req, res) => {
+    let recipeID = req.params.id;
+    Recipe.findById(recipeID).then(
+        (recipe) => {
+            if (recipe != null)
+                res.status(200).json({ message: 'Found', recipe });
+            else
+                res.status(404).json({ message: 'Not found' });
+        }
+    ).catch(
+        () => {
+            res.status(404).json({ message: 'Not found' });
+        }
+    )
+}
+
+const addRecipe = async (req, res) => {
     let recipe = new Recipe({
         name: req.body.name,
         descripition: req.body.descripition,
         imageURL: req.body.imageURL
     })
-    recipe.save();
-
-    res.status(201).json({ message: 'Added successfully' });
+    recipe.save().then(
+        (recipe) => {
+            res.status(201).json({ message: 'Added successfully', id: recipe._id });
+        }
+    )
+        .catch(
+            () => {
+                res.status(404).json({ message: 'Couldn\'t add recipe' });
+            }
+        )
 }
 
-module.exports = { getAllRecipes, addRecipe }
+module.exports = { getAllRecipes, addRecipe, getRecipe }
--- routes/recipes.js
+++ routes/recipes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const recipeController = require('../controllers/recipe');
 
+router.get('/:id', recipeController.getRecipe)
 router.get('', recipeController.getAllRecipes)
 router.post('', recipeController.addRecipe)
 
